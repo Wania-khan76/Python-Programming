a7d015be69b237050d06d8ae9599efcfb0081902
Wania Khan <131209600+Wania-khan76@users.noreply.github.com>
2024-02-13 09:37:33 +0500

    Create conditional statements.py

---
+++ conditional statements.py
@@ -0,0 +1,18 @@
+x=59
+y=20
+if x<y:
+    print("y is greater")
+else:
+     print("x is greater")
+
+
+
+x=9
+y=20
+if x<y:
+    print("y is greater")
+    if x==9:
+        print("value of x is 9")
+else:
+     print("x is greater")
+    
